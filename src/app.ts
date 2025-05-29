6c8bb5412cb8d9b1ed48dbc66e2e418a5ea27fec
mayur bhoi <mayurbhoi200@gmail.com>
2025-05-29 11:22:11 +0530

    add serviceAccount console

--- src/app.ts
+++ src/app.ts
@@ -18,11 +18,14 @@ dotenv.config();
 
 
 
+
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
+
+cosole.log("serviceAccount", serviceAccount)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
