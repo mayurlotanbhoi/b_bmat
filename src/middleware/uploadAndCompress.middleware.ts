817ca98361a474b6fc0ca8151f25843e0ac0a1ec
mayur lotan bbhoi <mayurbhoi200@gmail.com>
2025-09-15 00:33:32 +0530

    correct file upload

--- src/middleware/uploadAndCompress.middleware.ts
+++ src/middleware/uploadAndCompress.middleware.ts
@@ -69,7 +69,7 @@ export const uploadAndCompressImages = (req: Request, res: Response, next: NextF
 
         try {
             const files = req.files as Record<string, Express.Multer.File[]> || {};
-            const domain = process.env.DOMAIN_NAME || 'https://bhoi.joodi.in';
+            const domain = process.env.DOMAIN_NAME || 'https://api.joodi.in';
 
             const images = files['images'] || [];
             const verificationImages = files['verificationImage'] || [];
